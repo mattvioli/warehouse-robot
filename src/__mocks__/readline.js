a6f829b9907920e2fa62ed93c971afc255153860
mattvioli <mattvioli@gmail.com>
2023-07-29 14:57:31 +1000

    fix: Moved mocks into src

---
+++ src/__mocks__/readline.js
@@ -0,0 +1,11 @@
+"use strict";
+// mocking like this is unfavourable, as it gives us less control for future uses, however, mocking user inputs in the CLI is hard
+module.exports = {
+    createInterface: jest.fn().mockReturnValue({
+        question: jest
+            .fn()
+            .mockImplementationOnce((_questionTest, cb) => cb("N N W W")),
+        close: jest.fn().mockImplementationOnce(() => undefined),
+    }),
+};
+//# sourceMappingURL=readline.js.map
